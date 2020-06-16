6939b39a6bb2b9b34626d5c129aad2c89653bb16
Nick Olinger <olingern@gmail.com>
2020-06-15 20:55:03 -0400

    serve truncated response and golang tarball

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
 const http = require("http");
 const fs = require("fs");
 
+const tarballLoc = "./go-go1.15beta1.tar.gz";
+
 // Keep track of number of requests
 let i = 0;
+
+// Send invalid, truncated response first
+let truncatedSent = false;
 
 http
   .createServer((req, res) => {
     console.log(`Iteration ${i} `);
     // Fail
-    if (i < 5) {
+    if (i < 1) {
       res.statusCode = 500;
       res.end(`Fake error`);
       i++;
     } else {
       // Serve tarball
-      fs.readFile("./octokit.tar.gz", function (err, data) {
-        res.setHeader("Content-type", "application/tar+gzip");
-        res.end(data);
-      });
+
+      if (!truncatedSent) {
+        fs.readFile(tarballLoc, function (err, data) {
+          res.setHeader("Content-type", "application/tar+gzip");
+
+          truncatedSent = true;
+          res.end("truncated response");
+        });
+      } else {
+        fs.readFile(tarballLoc, function (err, data) {
+          res.setHeader("Content-type", "application/tar+gzip");
+
+          res.end(data);
+        });
+      }
     }
   })
   .listen(3000);
